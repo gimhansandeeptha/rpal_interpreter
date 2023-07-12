ee704df1fd97f50b26f86180c5312c3efaf77a2d
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2023-07-12 23:20:06 +0530

    complete scanner(not tested for bugs)

--- Scanner/src/RegEx.java
+++ Scanner/src/RegEx.java
@@ -6,14 +6,17 @@ public class RegEx {
     private static final String Operator_symbol= "+-*<>&.@/:=~|$!#%^_[]{}\\\"'?";
     private static final String LeftSqBracket = "[";
     private static final String RightSqBracket = "]"; 
-    private static final String Comment = "\"();,\\\\s\\t";  
+    private static final String Comment = "\"();,\\\\s\\t"; 
+    private static final String Punction = "();,";
 
 
     public static final Pattern LetterRegex = Pattern.compile(LeftSqBracket+Letter+RightSqBracket);
     public static final Pattern DigitRegex = Pattern.compile(Digit);
-    public static final Pattern Identifier = Pattern.compile(LeftSqBracket+Letter+Digit+RightSqBracket);
-    public static final Pattern Spaces = Pattern.compile(LeftSqBracket+"\\s\\t\\n"+RightSqBracket);
+    public static final Pattern IdentifierRegex = Pattern.compile(LeftSqBracket+Letter+Digit+RightSqBracket);
+    public static final Pattern SpacesRegex = Pattern.compile(LeftSqBracket+"\\s\\t\\n"+RightSqBracket);
     public static final Pattern CommentRegex = Pattern.compile(LeftSqBracket+Comment+Letter+Digit+Operator_symbol+RightSqBracket);
-    public static final Pattern Punction = Pattern.compile(LeftSqBracket+"();,"+RightSqBracket);
-    public static final Pattern Operator = Pattern.compile(Operator_symbol);
+    public static final Pattern PunctionRegex = Pattern.compile(LeftSqBracket+"();,"+RightSqBracket);
+    public static final Pattern OperatorRegex = Pattern.compile(Operator_symbol);
+    public static final Pattern StringRegex = Pattern.compile(LeftSqBracket+"\\s\\t\\n\\\\"+Punction+Letter+Digit+Operator_symbol+RightSqBracket);
+    
 }
--- Scanner/src/Scanner.java
+++ Scanner/src/Scanner.java
@@ -4,8 +4,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.List;
 
 public class Scanner {
     private BufferedReader buffer;
@@ -49,8 +47,135 @@ public class Scanner {
         return c;
     }
 
-    private Token buildToken(String c){
-        return null;
+    private Token buildToken(String c) throws IOException{
+        Token nextToken = null;
+        if (RegEx.LetterRegex.matcher(c).matches()) nextToken = identifierToken(c);
+        else if (RegEx.DigitRegex.matcher(c).matches()) nextToken = integerToken(c);
+        else if (RegEx.OperatorRegex.matcher(c).matches()) nextToken = operatorToken(c);
+        else if (RegEx.SpacesRegex.matcher(c).matches()) nextToken = spaceToken(c);
+        else if (RegEx.PunctionRegex.matcher(c).matches()) nextToken = punctionToken(c);
+        else if (c.equals("\\")) nextToken = commentToken(c);
+        else if (c.equals("\'")) nextToken = stringToken(c);
+
+        return nextToken;
+    }
+
+
+    private Token identifierToken(String c) throws IOException{
+        Token identifier = new Token();
+        identifier.setTokenType(TokenType.IDENTIFIER);
+        identifier.setTokenLine(lineNumber);
+        StringBuilder stringBuilder = new StringBuilder(c);
+        String nextChar = readNextChar();
+
+        while(RegEx.IdentifierRegex.matcher(nextChar).matches()){
+            stringBuilder.append(nextChar);
+            nextChar = readNextChar();
+        }
+        extraChar=nextChar;
+        identifier.setToken(stringBuilder.toString());
+
+        return identifier;
+    }
+
+    private Token integerToken(String c) throws IOException{
+        Token integer = new Token();
+        integer.setTokenType(TokenType.INTEGER);
+        integer.setTokenLine(lineNumber);
+        StringBuilder stringBuilder = new StringBuilder(c);
+        String nextChar = readNextChar();
+
+        while(RegEx.DigitRegex.matcher(nextChar).matches()){
+            stringBuilder.append(nextChar);
+            nextChar = readNextChar();
+        }
+        extraChar = nextChar;
+        integer.setToken(stringBuilder.toString());
+        return integer;
+    }
+
+    private Token operatorToken(String c) throws IOException{
+        Token operator = new Token();
+        operator.setTokenType(TokenType.OPERATOR);
+        operator.setTokenLine(lineNumber);
+        StringBuilder stringBuilder = new StringBuilder(c);
+        String nextChar = readNextChar();
+
+        while(RegEx.OperatorRegex.matcher(nextChar).matches()){
+            stringBuilder.append(nextChar);
+            nextChar = readNextChar();
+        }
+        extraChar = nextChar;
+        operator.setToken(stringBuilder.toString());
+        return operator;
+    }
+
+    private Token stringToken(String c) throws IOException{
+        Token string  = new Token();
+        string.setTokenType(TokenType.STRING);
+        string.setTokenLine(lineNumber);
+        StringBuilder stringBuilder = new StringBuilder(c);
+        String nextChar = readNextChar();
+
+        while(RegEx.StringRegex.matcher(nextChar).matches()){
+            if (nextChar == "\'") break;
+            stringBuilder.append(nextChar);
+            nextChar = readNextChar();
+        }
+        stringBuilder.append(nextChar);
+        string.setToken(stringBuilder.toString());
+        return string;
+    }
+
+    private Token spaceToken(String c) throws IOException{
+        Token space = new Token();
+        space.setTokenType(TokenType.DELETE);
+        space.setTokenLine(lineNumber);
+        StringBuilder stringBuilder = new StringBuilder(c);
+        String nextChar = readNextChar();
+
+        while(RegEx.SpacesRegex.matcher(nextChar).matches()){
+            stringBuilder.append(nextChar);
+            nextChar = readNextChar();
+        }
+        extraChar = nextChar;
+        space.setToken(stringBuilder.toString());
+        return space;
+    }
+
+    private Token commentToken(String c) throws IOException{
+        Token comment = new Token();
+        comment.setTokenType(TokenType.DELETE);
+        comment.setTokenLine(lineNumber);
+        StringBuilder stringBuilder = new StringBuilder(c);
+        String nextChar = readNextChar();
+
+        while(RegEx.CommentRegex.matcher(nextChar).matches()){
+            if (nextChar == "\n") break;
+            stringBuilder.append(nextChar);
+            nextChar = readNextChar();
+        }
+        comment.setToken(stringBuilder.toString());
+        return comment;
+    }
+
+    private Token punctionToken(String c) throws IOException {
+        Token punction = new Token();
+        punction.setTokenLine(lineNumber);
+        StringBuilder stringBuilder = new StringBuilder();
+        String nextChar = c;
+
+        while(RegEx.PunctionRegex.matcher(nextChar).matches()){
+            if (nextChar.equals("(")) punction.setTokenType(TokenType.L_PAREN);
+            else if (nextChar.equals(")")) punction.setTokenType(TokenType.R_PAREN);
+            else if (nextChar.equals(";")) punction.setTokenType(TokenType.SEMICOLON);
+            else if (nextChar.equals(",")) punction.setTokenType(TokenType.COMMA);
+            stringBuilder.append(nextChar);
+            nextChar = readNextChar();
+        }
+        extraChar = nextChar;
+        punction.setToken(stringBuilder.toString());
+        return punction;
     }
 
 
