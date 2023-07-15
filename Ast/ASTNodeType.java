9dfd1dcb9a3e11151e22a6d4d319b087d8887402
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2023-07-15 12:34:03 +0530

    Update ASTNodeType.java

--- Ast/ASTNodeType.java
+++ Ast/ASTNodeType.java
@@ -1,5 +1,60 @@
 package Ast;
 
-public class ASTNodeType {
-    
+public enum ASTNodeType {
+
+    IDENTIFIER,
+    STRING,
+    INTEGER,
+
+    LET,
+    LAMBDA,
+    WHERE,
+    TAU,
+    AUG,
+    CONDITIONAL,
+
+    OR,
+    BOOLAND,
+    NOT,
+    GR,
+    GE,
+    LS,
+    LE,
+    EQ,
+    NE,
+
+    //Arithmatic expressions 
+    PLUS,
+    MINUS,
+    NEG,
+    MUL,
+    DEV,
+    EXP,
+    AT,
+
+    //Operators and operands 
+    GAMMA,
+    TRUE,
+    FALSE,
+    NIL,
+    DUMMY,
+
+    //Definitions 
+    WITHIN,
+    AND,
+    REC,
+    EQUAL,
+    FCNFORM,
+
+    //Variables 
+    PARAN,
+    COMMA,
+
+    Y,
+
+    BETA,
+    DELTA,
+    ETA,
+    TUPLE;
+
 }
